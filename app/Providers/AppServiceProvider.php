b8bddcbd6150ec01c64d9dae6b68a21f8c9a5096
pa360800-cmyk <pa360800@example.com>
2026-07-27 14:39:56 +0800

    RAILWAY_ENVIRONMENT

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (app()->environment('production')) {
+        if (isset($_ENV['RAILWAY_ENVIRONMENT']) || isset($_SERVER['RAILWAY_ENVIRONMENT'])) {
             URL::forceScheme('https');
             URL::forceRootUrl(config('app.url'));
         }
